3a032fe7ff5f308e47297564ee396c445e63154e
suin0704 <fropop74@gmail.com>
2023-08-10 23:34:07 +0900

    0810 스프링프레임워크 첫걸음

---
+++ SpringMVCModelSample/src/test/java/com/example/SpringMVCModelSample/SpringMvcModelSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringMVCModelSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcModelSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
